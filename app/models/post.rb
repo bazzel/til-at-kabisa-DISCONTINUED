7367fd220c231f1bbe4864cd9bab0d342172e036
Patrick Baselier <patrick.baselier@gmail.com>
2021-07-29 18:06:29 +0200

    Add slug

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
+  after_validation :set_slug, only: [:create, :update]
+
+  def to_param
+    "#{id}-#{slug}"
+  end
+
+  private
+    def set_slug
+      self.slug = title.to_s.parameterize
+    end
 end
